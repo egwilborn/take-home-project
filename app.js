3356ba9045535e61b08b554bf0550e9a59e7ff98
egwilborn <egwilborn@gmail.com>
2024-02-15 17:30:08 -0600

    logs parsed message

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+//JSON Data
 const companies = require("./Companies.json");
+const guests = require("./Guests.json");
+const messages = require("./Messages.json");
+//Packages
+const parse = require("json-templates");
 
-console.log(companies);
+//testing variables
+const greeting = "hi";
+const firstName = "hayley";
+const company = "Hilton";
+const roomNumber = "304";
+
+// using package
+const template = parse(messages[0].message);
+console.log(
+  template({
+    greeting: greeting,
+    firstName: firstName,
+    roomNumber: roomNumber,
+    company: company,
+  })
+);
