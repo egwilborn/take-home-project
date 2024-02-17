5b98ed2aeecdaccc5096b8deb16b082826337994
egwilborn <egwilborn@gmail.com>
2024-02-16 21:07:26 -0600

    finalizes code with comments for clarification and organization

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const prompt = require("prompt");
 //prompt input for company, guest, and message using prompt package
 prompt.start();
 console.log(
-  `Please input the id number of the hotel (# 1-${companies.length}) and guest (# 1-${guests.length1}) you would like to contact. Then decide which of the following messages you would like to send: (1) Room is ready, (2) Welcome to the city, (3) Room is not ready, (4) Food recommendations, (5) Custom message`
+  `Please input the id number of the hotel (# 1-${companies.length}) and guest (# 1-${guests.length}) you would like to contact. Then decide which of the following messages you would like to send: (1) Room is ready, (2) Welcome to the city, (3) Room is not ready, (4) Food recommendations, (5) Custom message`
 );
 prompt.get(["companyId", "guestId", "messageId"], function (err, result) {
+  if (err) console.log("prompt error:", err);
+  //state variables
   const companyId = result.companyId;
   const guestId = result.guestId;
   const messageId = result.messageId;
@@ -27,13 +29,12 @@ prompt.get(["companyId", "guestId", "messageId"], function (err, result) {
     timeZoneName: "long",
   };
   const currentTime = currentTimeStamp.toLocaleTimeString("en-US", timeZone);
-  console.log(currentTime);
 
   // determine greeting based on time
   const hour = parseInt(currentTime);
   if (currentTime.includes("AM")) {
     greeting = "Good morning";
-  } else if (currentTime.includes("PM") && hour > 6) {
+  } else if (currentTime.includes("PM") && hour >= 6) {
     greeting = "Good evening";
   } else {
     greeting = "Good afternoon";
@@ -54,18 +55,15 @@ prompt.get(["companyId", "guestId", "messageId"], function (err, result) {
     const messageTemplate = parse(messages[messageId - 1].message);
     console.log(messageTemplate(reservationInfo));
   } else {
-    // ask for custom message based on number
+    // provide user with guest and company info
     console.log(
       `Write your message to ${reservationInfo.firstName} ${reservationInfo.lastName} in room ${reservationInfo.roomNumber} at ${reservationInfo.company}`
     );
+    // provide user with input for custom message
     prompt.get(["message"], function (err, result) {
+      if (err) console.log("prompt error:", err);
+      // log that message back for them
       console.log(result.message);
     });
   }
 });
-
-// ------- NEEDS the Following ------- //
-// fix the greeting time problem
-// consider adding tests or notes for tests
-// add a readme
-// currently at 4-5 hours of work
